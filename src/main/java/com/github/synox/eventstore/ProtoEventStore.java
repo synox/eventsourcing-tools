653f0e4dd8d9a7345fee582aed335a81c5b3ee5e
Synox <synox@users.noreply.github.com>
2017-11-25 22:07:49 +0100

    cleanup

--- src/main/java/com/github/synox/eventstore/ProtoEventStore.java
+++ src/main/java/com/github/synox/eventstore/ProtoEventStore.java
@@ -1,7 +1,6 @@
 package com.github.synox.eventstore;
 
 import com.google.protobuf.GeneratedMessageV3;
-import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Parser;
 import journal.io.api.Journal;
 import journal.io.api.JournalBuilder;
@@ -18,44 +17,29 @@ import java.util.stream.StreamSupport;
  *
  * @param <T> the generated Protobuf message class
  */
-public class ProtoEventStore<T extends GeneratedMessageV3> {
+public class ProtoEventStore<T extends GeneratedMessageV3> implements AutoCloseable {
     private Journal journal;
     private Parser<T> parser;
-    private Path dir;
 
-    public ProtoEventStore(Parser<T> parser, Path dir) {
+    public ProtoEventStore(Parser<T> parser, Path dir) throws IOException {
         this.parser = parser;
-        this.dir = dir;
-    }
-
-    public void start() throws IOException {
         Files.createDirectories(dir);
         journal = JournalBuilder.of(dir.toFile()).open();
     }
 
-    public void stop() throws IOException {
+    @Override
+    public void close() throws IOException {
         journal.close();
     }
 
     public void append(T o) {
         try {
-            journal.write(serialize(o), Journal.WriteType.SYNC);
+            journal.write(o.toByteArray(), Journal.WriteType.SYNC);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private byte[] serialize(T o) {
-        return o.toByteArray();
-    }
-
-    private T deserialize(byte[] bytes) {
-        try {
-            return parser.parseFrom(bytes);
-        } catch (InvalidProtocolBufferException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public Stream<T> stream() throws IOException {
         return StreamSupport.stream(journal.redo().spliterator(), false)
@@ -65,9 +49,10 @@ public class ProtoEventStore<T extends GeneratedMessageV3> {
     private T readLocation(Location loc) {
         try {
             byte[] record = journal.read(loc, Journal.ReadType.SYNC);
-            return deserialize(record);
+            return parser.parseFrom(record);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
